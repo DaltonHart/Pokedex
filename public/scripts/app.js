dc90f80ae58aacf623dd4ecef099adfa6fd2716b
Dalton Hart <DaltonVaren@gmail.com>
2019-07-17 14:29:16 -0700

    adds init ajax call

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,40 @@
 console.log(`Gotta catch'em all!`);
+
+// Constant Variables
+const BASE_URL = '/api/pokemon';
+// Global Variables
+
+// State Variable
+const state = {
+	pokemon: []
+};
+
+// Dom Elements
+const $pokedex = $('#pokedex');
+// Functions
+
+const render = () => {
+	const { pokemon } = state;
+	pokemon.forEach(doc => {
+		console.log({ pokemon: doc });
+	});
+};
+
+const updateState = response => {
+	const { data } = response;
+	state.pokemon = data;
+	render();
+};
+
+const getAllPokemon = () => {
+	$.ajax({
+		url: BASE_URL,
+		method: 'GET',
+		success: updateState,
+		error: (e1, e2, e3) => console.log(e2)
+	});
+};
+
+getAllPokemon();
+
+// Event Listeners
