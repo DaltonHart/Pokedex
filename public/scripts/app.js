1b9d4d5b6a5680df103b6b72a43e435be49c7096
daltonhart <daltonvaren@gmail.com>
2020-01-12 15:34:40 -0800

    adds fix for frontend

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,13 @@
 console.log(`Gotta catch'em all!`);
 
 // Constant Variables
-const BASE_URL = '/api/pokemon';
+const BASE_URL = '/api/v1/pokemon';
 // Global Variables
 
 // State Variable
 const state = {
-	pokemon: [],
-	filtered: []
+  pokemon: [],
+  filtered: []
 };
 
 // Dom Elements
@@ -17,14 +17,14 @@ const $searchbar = $('#search-pokemon');
 // Functions
 
 const typeComponent = types => {
-	return types
-		.map(type => `<p class="type ${type.toLowerCase()}">${type}</p>`)
-		.join('');
+  return types
+    .map(type => `<p class="type ${type.toLowerCase()}">${type}</p>`)
+    .join('');
 };
 
 const pokemonComponent = pokemon => {
-	const types = typeComponent(pokemon.type);
-	return `
+  const types = typeComponent(pokemon.type);
+  return `
     <div class="card">
       <div class="img-container">
         <img class="card-img" src="${pokemon.image}" alt="${pokemon.name}"/>
@@ -45,47 +45,47 @@ const pokemonComponent = pokemon => {
 };
 
 const render = () => {
-	const { pokemon, filtered } = state;
-	$pokedex.empty();
-	if (filtered.length > 0) {
-		filtered.forEach(doc => {
-			const card = pokemonComponent(doc);
-			$pokedex.append(card);
-		});
-	} else {
-		pokemon.forEach(doc => {
-			const card = pokemonComponent(doc);
-			$pokedex.append(card);
-		});
-	}
+  const { pokemon, filtered } = state;
+  $pokedex.empty();
+  if (filtered.length > 0) {
+    filtered.forEach(doc => {
+      const card = pokemonComponent(doc);
+      $pokedex.append(card);
+    });
+  } else {
+    pokemon.forEach(doc => {
+      const card = pokemonComponent(doc);
+      $pokedex.append(card);
+    });
+  }
 };
 
 const updateState = response => {
-	const { data } = response;
-	state.pokemon = data;
-	render();
+  const { data } = response;
+  state.pokemon = data;
+  render();
 };
 
 const getAllPokemon = () => {
-	$.ajax({
-		url: BASE_URL,
-		method: 'GET',
-		success: updateState,
-		error: (e1, e2, e3) => console.log(e2)
-	});
+  $.ajax({
+    url: BASE_URL,
+    method: 'GET',
+    success: updateState,
+    error: (e1, e2, e3) => console.log(e2)
+  });
 };
 
 getAllPokemon();
 
 const filterPokemon = event => {
-	const filteredPokemon = state.pokemon.filter(pokemon => {
-		return (
-			pokemon.pokedex.toString() === event.target.value ||
-			pokemon.name.toLowerCase().includes(event.target.value.toLowerCase())
-		);
-	});
-	state.filtered = filteredPokemon;
-	render();
+  const filteredPokemon = state.pokemon.filter(pokemon => {
+    return (
+      pokemon.pokedex.toString() === event.target.value ||
+      pokemon.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+  });
+  state.filtered = filteredPokemon;
+  render();
 };
 
 // Event Listeners
